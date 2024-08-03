e90f10ffef5da0c6180f9e4ba3614d1736a4f919
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-08-02 19:49:10 -0600

    Initial Commit

---
+++ AppCurrency/AppCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCurrencyApp.swift
+//  AppCurrency
+//
+//  Created by Montserrat Gomez on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AppCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
