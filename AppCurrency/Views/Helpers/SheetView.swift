f2c4e05ac581c45ffcebbc868b6d9af4f49eb410
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-08-13 17:33:32 -0600

    Add API Rest logic

---
+++ AppCurrency/Views/Helpers/SheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetView.swift
+//  AppCurrency
+//
+//  Created by Montserrat Gomez on 13/08/24.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetView()
+}
